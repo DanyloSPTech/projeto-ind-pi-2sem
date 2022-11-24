9597ff817ef605fc244ce3b0d2603cd2bc7aaa3b
DanyloSPTech <danylo.gomes@sptech.school>
2022-11-24 01:22:47 -0300

    r script gerando png de wordcloud

--- RStudio/script_word_cloud.R
+++ RStudio/script_word_cloud.R
@@ -1,7 +1,15 @@
+#!/usr/bin/env Rscript --vanilla
+
 library(tm)
 library(wordcloud)
 library(RColorBrewer)
 library(wordcloud2)
+
+#install webshot
+library(webshot)
+webshot::install_phantomjs()
+
+words_scraper <- read.csv("words_scraper.csv")
 
 palavras <- Corpus(VectorSource(words_scraper$Palavra))
 
@@ -11,4 +19,10 @@ words <- sort(rowSums(matrizPalavras), decreasing = TRUE)
 
 dfFreqPalavras <- data.frame(word = names(words), freq = words)
 
-wordcloud2(data=dfFreqPalavras, size=0.7, color='random-light', backgroundColor = 'black', rotateRatio = 0, shape = 'circle')
+plotWordCloud <- wordcloud2(data=dfFreqPalavras, size=1, color='random-light', backgroundColor = 'black', rotateRatio = 0, shape = 'circle')
+
+## save it in html
+library("htmlwidgets")
+saveWidget(plotWordCloud,"CLOUD.html",selfcontained = F)
+
+webshot("CLOUD.html","CLOUD_1.png", delay = 5)
--- caca-reclamacao/main.py
+++ caca-reclamacao/main.py
@@ -14,90 +14,93 @@ from os import path
 from os import getcwd
 import csv
 from palavras_chave import palavras_chave
+import subprocess
 
-option = Options()
-option.headless = False
-driver = webdriver.Firefox(options=option, executable_path=GeckoDriverManager().install())
+# option = Options()
+# option.headless = False
+# driver = webdriver.Firefox(options=option, executable_path=GeckoDriverManager().install())
 
-i = 1
+# i = 1
 
-titulos = []
-textos = []
+# titulos = []
+# textos = []
 
-while i <= 25:
-    url_base = f"https://www.reclameaqui.com.br/empresa/c6-bank/lista-reclamacoes/?busca=pix&pagina={i}"
-    driver.get(url=url_base)
+# while i <= 25:
+#     url_base = f"https://www.reclameaqui.com.br/empresa/c6-bank/lista-reclamacoes/?busca=pix&pagina={i}"
+#     driver.get(url=url_base)
     
-    div_reclamacoes = driver.find_element(By.CLASS_NAME, "eFXbXn")
+#     div_reclamacoes = driver.find_element(By.CLASS_NAME, "eFXbXn")
 
-    cont_html = div_reclamacoes.get_attribute('outerHTML')
-    sopa = BeautifulSoup(cont_html, 'html.parser')
+#     cont_html = div_reclamacoes.get_attribute('outerHTML')
+#     sopa = BeautifulSoup(cont_html, 'html.parser')
 
-    lista_reclamacao = sopa.find_all('div', class_ = 'bJdtis')
+#     lista_reclamacao = sopa.find_all('div', class_ = 'bJdtis')
 
-    # print(lista_reclamacao)
+#     # print(lista_reclamacao)
 
-    for reclamacao in lista_reclamacao:
-        titulo = reclamacao.select('a h4')
-        texto = reclamacao.select('p')
+#     for reclamacao in lista_reclamacao:
+#         titulo = reclamacao.select('a h4')
+#         texto = reclamacao.select('p')
         
-        titulosUnicos = []
-        textosUnicos = []
+#         titulosUnicos = []
+#         textosUnicos = []
         
-        tituloStr = str(titulo[0].get_text())
-        palavrasTi = tituloStr.split()
-        for palavra in palavrasTi:
-            palavraTratada = palavra.lower()
-            print(palavraTratada + '\n-------------')
-            titulosUnicos.append(palavraTratada)
+#         tituloStr = str(titulo[0].get_text())
+#         palavrasTi = tituloStr.split()
+#         for palavra in palavrasTi:
+#             palavraTratada = palavra.lower()
+#             print(palavraTratada + '\n-------------')
+#             titulosUnicos.append(palavraTratada)
 
-        textoStr = str(texto[0].get_text())
-        palavrasTe = textoStr.split()
-        for palavra in palavrasTe:
-            palavraTratada = palavra.lower()
-            print(palavraTratada + '\n-------------')
-            textosUnicos.append(palavraTratada)
+#         textoStr = str(texto[0].get_text())
+#         palavrasTe = textoStr.split()
+#         for palavra in palavrasTe:
+#             palavraTratada = palavra.lower()
+#             print(palavraTratada + '\n-------------')
+#             textosUnicos.append(palavraTratada)
             
-        for palavra in titulosUnicos:
-            for x in palavras_chave:
-                if palavra == x:
-                    titulos.append(palavra)
+#         for palavra in titulosUnicos:
+#             for x in palavras_chave:
+#                 if palavra == x:
+#                     titulos.append(palavra)
                     
-                    caminho = getcwd()
+#                     caminho = getcwd()
                 
-                    if path.isfile(f'{caminho}\words_scraper.csv'):
-                        with open('words_scraper.csv', 'a', newline='', encoding='UTF8') as arquivo:
-                            writer = csv.DictWriter(arquivo, fieldnames=['Palavra'])
-                            writer.writerow({'Palavra': palavra})
-                    else:
-                        with open('words_scraper.csv', 'w', newline='', encoding='UTF8') as arquivo:
-                            writer = csv.DictWriter(arquivo, fieldnames=['Palavra'])
-                            writer.writeheader()
-                            writer.writerow({'Palavra': palavra})
+#                     if path.isfile(f'{caminho}\words_scraper.csv'):
+#                         with open('words_scraper.csv', 'a', newline='', encoding='UTF8') as arquivo:
+#                             writer = csv.DictWriter(arquivo, fieldnames=['Palavra'])
+#                             writer.writerow({'Palavra': palavra})
+#                     else:
+#                         with open('words_scraper.csv', 'w', newline='', encoding='UTF8') as arquivo:
+#                             writer = csv.DictWriter(arquivo, fieldnames=['Palavra'])
+#                             writer.writeheader()
+#                             writer.writerow({'Palavra': palavra})
                 
-        for palavra in textosUnicos:
-            for x in palavras_chave:
-                if palavra == x:
-                    textos.append(palavra)
+#         for palavra in textosUnicos:
+#             for x in palavras_chave:
+#                 if palavra == x:
+#                     textos.append(palavra)
                     
-                    caminho = getcwd()
+#                     caminho = getcwd()
                 
-                    if path.isfile(f'{caminho}\words_scraper.csv'):
-                        with open('words_scraper.csv', 'a', newline='', encoding='UTF8') as arquivo:
-                            writer = csv.DictWriter(arquivo, fieldnames=['Palavra'])
-                            writer.writerow({'Palavra': palavra})
-                    else:
-                        with open('words_scraper.csv', 'w', newline='', encoding='UTF8') as arquivo:
-                            writer = csv.DictWriter(arquivo, fieldnames=['Palavra'])
-                            writer.writeheader()
-                            writer.writerow({'Palavra': palavra})
+#                     if path.isfile(f'{caminho}\words_scraper.csv'):
+#                         with open('words_scraper.csv', 'a', newline='', encoding='UTF8') as arquivo:
+#                             writer = csv.DictWriter(arquivo, fieldnames=['Palavra'])
+#                             writer.writerow({'Palavra': palavra})
+#                     else:
+#                         with open('words_scraper.csv', 'w', newline='', encoding='UTF8') as arquivo:
+#                             writer = csv.DictWriter(arquivo, fieldnames=['Palavra'])
+#                             writer.writeheader()
+#                             writer.writerow({'Palavra': palavra})
                         
-    print("Titulos: \n")
-    for palavra in titulos:
-        print(palavra + '\n')
-    print("\n\nTextos:\n")
-    for palavra in textos:
-        print(palavra + '\n')
+#     print("Titulos: \n")
+#     for palavra in titulos:
+#         print(palavra + '\n')
+#     print("\n\nTextos:\n")
+#     for palavra in textos:
+#         print(palavra + '\n')
         
         
-    i += 1
+#     i += 1
+    
+subprocess.call(["E:/Users/Danyl/Documents/GitHub/projeto-ind-pi-2sem/RStudio/script_word_cloud.R", "--vanilla"], shell=True)
